3045ebdfc09b6c3076b7b4938646c34274fab138
okey E <okeysmooed@gmail.com>
2024-02-02 00:15:01 +0000

    update

--- src/services/getPokemon.js
+++ src/services/getPokemon.js
@@ -1,12 +1,13 @@
-const baseUrl = "https://pokeapi.co/api/v2/";
+
+const baseUrl = 'http://pokeapi.co/api/v2';
 const query = {
-  pokemon: "pokemon",
+  pokemon: 'pokemon',
   type: "type",
   ability: "ability",
   stat: 'stat',
-};
+}
 
 export async function fetchPokemon(pokemon) {
   console.log(`${baseUrl}/${query.pokemon}/${pokemon}`);
-  return fetch(`${baseUrl}/${query.pokemon}/${pokemon}`);
+  return fetch(`${baseUrl}/${query.pokemon}/${pokemon}`)
 }
